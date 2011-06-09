496c19f5698143868848eb778a835825dba8e94e
Rene Hopf <renehopf@mac.com>
2011-06-09 18:35:48 +0200

    fixed GUI

--- zwitscher.go
+++ zwitscher.go
@@ -5,5 +5,6 @@ import "gotter"
 func main() {
 	Connect()
 	gotter.PostTweet(accounts.Credentials, "https://api.twitter.com/1/statuses/update.json", map[string]string{"status": "Eat this, @simonszu ! Hard codet Tweets!"})
+	Gui()
 }
 
